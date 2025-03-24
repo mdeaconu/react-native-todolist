1e49c320fbb79925057e0d1c8a4000eb0aa8cc7d
Mihai DEACONU <mihai.deaconu@cognizant.com>
2025-03-24 14:15:38 +0200

    feat: layout
    Signed-off-by: Mihai DEACONU <mihai.deaconu@cognizant.com>

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { s } from "./App.style";
 
-export default function App() {
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>hello</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={s.app}>
+          <View style={s.header}>
+            <Text>Header</Text>
+          </View>
+          <View style={s.body}>
+            <Text>Body</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={s.footer}>
+        <Text>Footer</Text>
+      </View>
+    </>
   );
-}
+};
 
+export default App;
