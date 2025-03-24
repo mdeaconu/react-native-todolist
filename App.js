ece963f795a57264c1bbb9ab929a418b29088926
Mihai DEACONU <mihai.deaconu@cognizant.com>
2025-03-24 15:22:58 +0200

    feat: card component
    Signed-off-by: Mihai DEACONU <mihai.deaconu@cognizant.com>

--- App.js
+++ App.js
@@ -3,6 +3,13 @@ import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
 import Header from "./components/Header/Header";
+import CardTodo from "./components/CardTodo/CardTodo";
+
+const TODO_LIST = [
+  { id: 1, title: "Walk the dog", isCompleted: true },
+  { id: 2, title: "Go to the dentist", isCompleted: false },
+  { id: 3, title: "Learn React Native", isCompleted: false },
+];
 
 const App = () => {
   return (
@@ -13,7 +20,7 @@ const App = () => {
             <Header />
           </View>
           <View style={s.body}>
-            <Text>Body</Text>
+            <CardTodo todo={TODO_LIST[1]} />
           </View>
         </SafeAreaView>
       </SafeAreaProvider>
---
+++ components/CardTodo/CardTodo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { s } from "./CardTodo.sytle";
+import { Image, Text, TouchableOpacity } from "react-native";
+import checkMark from "../../assets/check.png";
+
+const CardTodo = ({ todo }) => {
+  return (
+    <TouchableOpacity style={s.card}>
+      <Text style={[s.title, todo.isCompleted && { textDecorationLine: "line-through" }]}>
+        {todo.title}
+      </Text>
+      {todo.isCompleted && <Image style={s.image} source={checkMark} />}
+    </TouchableOpacity>
+  );
+};
+
+export default CardTodo;
